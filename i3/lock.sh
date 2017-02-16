a166f93c11f09c372e786f8022f718fb9f9cb795
sho <shomiyauchix@gmail.com>
2017-02-16 14:14:55 -0800

    changed lock icon directory

--- i3/lock.sh
+++ i3/lock.sh
@@ -9,7 +9,7 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
     PX=0
     PY=0
     # lockscreen image info
-    R=$(file ~/.config/screen-lock.png | grep -o '[0-9]* x [0-9]*')
+    R=$(file ~/.config/i3/screen-lock.png | grep -o '[0-9]* x [0-9]*')
     RX=$(echo $R | cut -d' ' -f 1)
     RY=$(echo $R | cut -d' ' -f 3)
 
@@ -24,10 +24,10 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
         PX=$(($SROX + $SRX/2 - $RX/2))
         PY=$(($SROY + $SRY/2 - $RY/2))
 
-        convert /tmp/screen.png $HOME/.config/screen-lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
+        convert /tmp/screen.png $HOME/.config/i3/screen-lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
         echo "done"
     done
 #fi 
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 # i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
-i3lock -e -u -n -i /tmp/screen.png
+i3lock -e -n -i /tmp/screen.png
